17c8b57e242ab61af4672ad41e4c741a4e5c8785
Harakazu <nftug@users.noreply.github.com>
2026-01-26 04:19:45 +0900

    update color theme variables and adjust layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 
 <ThemeProvider>
   <Navbar
-    class="px-4 py-0 bg-white dark:border-gray-700 dark:bg-gray-900"
+    class="px-4 py-0 border-b border-gray-200 dark:border-zinc-700"
     navContainerClass="h-16 items-center"
   >
     <NavBrand href="/" class="text-xl font-semibold">My Life Logger</NavBrand>
@@ -20,7 +20,7 @@
     <DarkMode />
   </Navbar>
 
-  <main style="height: calc(100vh - 4rem);" class="mx-auto overflow-auto">
+  <main style="height: calc(100vh - 4rem - 1px);" class="mx-auto overflow-auto">
     {@render children()}
   </main>
 </ThemeProvider>
