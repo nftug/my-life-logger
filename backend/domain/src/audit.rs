de2c13064323bb56e72c6f60aab31477cd291d81
Harakazu <nftug@users.noreply.github.com>
2026-02-01 16:14:18 +0900

    refactor: moved AppTimeZone module to single file

--- backend/domain/src/audit.rs
+++ backend/domain/src/audit.rs
@@ -1,5 +1,7 @@
+pub mod app_timezone;
 pub mod audit_context;
 pub mod clock;
 
+pub use app_timezone::AppTimeZone;
 pub use audit_context::AuditContext;
 pub use clock::Clock;
---
+++ backend/domain/src/audit/app_timezone.rs
@@ -0,0 +1,34 @@
+use chrono::{DateTime, FixedOffset, Local, NaiveDate, NaiveDateTime, TimeZone, Utc};
+
+#[derive(Debug, Clone)]
+pub enum AppTimeZone {
+    Utc,
+    Local,
+    Fixed(FixedOffset),
+}
+
+impl AppTimeZone {
+    pub fn naive_date(&self, datetime: DateTime<Utc>) -> NaiveDate {
+        match self {
+            AppTimeZone::Utc => datetime.date_naive(),
+            AppTimeZone::Local => datetime.with_timezone(&Local).date_naive(),
+            AppTimeZone::Fixed(offset) => datetime.with_timezone(offset).date_naive(),
+        }
+    }
+
+    pub fn start_of_date(&self, date: NaiveDate) -> DateTime<Utc> {
+        self.utc_datetime(date.and_hms_opt(0, 0, 0).unwrap())
+    }
+    pub fn start_of_next_date(&self, date: NaiveDate) -> DateTime<Utc> {
+        self.start_of_date(date.succ_opt().unwrap())
+    }
+    pub fn utc_datetime(&self, naive_datetime: NaiveDateTime) -> DateTime<Utc> {
+        match self {
+            AppTimeZone::Utc => Utc.from_utc_datetime(&naive_datetime),
+            AppTimeZone::Local => Local.from_local_datetime(&naive_datetime).unwrap().into(),
+            AppTimeZone::Fixed(offset) => {
+                offset.from_local_datetime(&naive_datetime).unwrap().into()
+            }
+        }
+    }
+}
